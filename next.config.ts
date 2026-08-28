6c7d03bb29e3de92fa89657809a2d7c49fe637df
A Baim <baimwarunkarsi@gmail.com>
2026-08-28 16:57:19 +0700

    fix(deploy): fix config files

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-import type { NextConfig } from "next";
-
+﻿import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   experimental: {
     serverActions: {
       bodySizeLimit: '5mb',
@@ -21,5 +14,4 @@ const nextConfig: NextConfig = {
     ],
   },
 };
-
 export default nextConfig;
